53bb32d45a1f23435212c1e70d88ab0f72effb99
Geovanny  Gómez Leiva <geovanny.gomez@pandatechla.com>
2024-12-06 12:37:07 -0600

    fix/API response modification

--- src/main/java/com/example/demo_sprint_boot/rest/controller/HelloController.java
+++ src/main/java/com/example/demo_sprint_boot/rest/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! It's a pleasure.";
+        return "Hola polluela hermosa, espero que este teniendo un hermoso día. Te amo <3";
     }
 }
