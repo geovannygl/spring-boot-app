d8b60452bc5b8305e215b43c998b001bddee21e7
Geovanny  Gómez Leiva <geovanny.gomez@pandatechla.com>
2024-12-09 17:20:42 -0600

    add/Added new environment variable to get the branch when the changes are applied

--- src/main/java/com/example/demo_sprint_boot/rest/controller/HelloController.java
+++ src/main/java/com/example/demo_sprint_boot/rest/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hi from Spring Boot Application!";
+        return "Hi from Spring Boot Application! Now I'm using environment variables";
     }
 }
